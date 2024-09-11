24ce1aa534b616aa9934b8c3d95c86a2236d8a20
Brendan Carlson <brendancarlson1125@gmail.com>
2024-09-11 14:55:31 -0500

    refactor file structure

---
+++ load.go
@@ -0,0 +1,119 @@
+package genv
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"strings"
+)
+
+type entry struct {
+	value string
+	typ   string
+}
+
+func Load(filenames ...string) error {
+	if err := load(filenames...); err != nil {
+		return err
+	}
+	return nil
+}
+
+func load(filenames ...string) error {
+	vars := make(map[string]*entry)
+
+	if len(filenames) == 0 {
+		if err := loadFile(".env", vars); err != nil {
+			return err
+		}
+	}
+
+	for _, filename := range filenames {
+		if err := loadFile(filename, vars); err != nil {
+			return err
+		}
+	}
+
+	if err := expand(vars); err != nil {
+		return err
+	}
+
+	for k, v := range vars {
+		os.Setenv(k, v.value)
+	}
+
+	return nil
+}
+
+func loadFile(filename string, vars map[string]*entry) error {
+	f, err := os.Open(filename)
+	if err != nil {
+		return fmt.Errorf("genv: %w", err)
+	}
+	defer func() {
+		if err := f.Close(); err != nil {
+			log.Fatalf("genv: close: %v", err)
+		}
+	}()
+
+	scanner := bufio.NewScanner(f)
+	for scanner.Scan() {
+		if err = scanner.Err(); err != nil {
+			return fmt.Errorf("genv: parse: %w", err)
+		}
+		line := strings.TrimSpace(scanner.Text())
+
+		if len(line) == 0 {
+			continue
+		} else if line[0] == '#' {
+			continue
+		}
+
+		// TODO: handle type annotations after value e.g. INT_KEY=16 #int
+		// TODO: handle escaped values
+		key, rest, found := strings.Cut(line, "=")
+		if !found {
+			continue
+		}
+
+		value, typ, found := strings.Cut(rest, "#")
+		if !found {
+			typ = "string"
+		}
+
+		key = strings.TrimSpace(key)
+		value = strings.TrimSpace(value)
+		if value[0] == '"' && value[len(value)-1] == '"' {
+			value = strings.Trim(value, "\"")
+		}
+		typ = strings.TrimSpace(typ)
+
+		vars[key] = &entry{value, typ}
+	}
+
+	return nil
+}
+
+var expandRegexp = regexp.MustCompile(`(\${(.*)})`)
+
+func expand(vars map[string]*entry) error {
+	for _, v := range vars {
+		matches := expandRegexp.FindAllStringSubmatch(v.value, -1)
+		if len(matches) == 0 {
+			continue
+		}
+		for _, match := range matches {
+			if len(match) != 3 {
+				continue
+			}
+			if ev, ok := vars[match[2]]; ok {
+				v.value = strings.Replace(v.value, match[1], ev.value, 1)
+			} else {
+				return fmt.Errorf("genv: undeclared environment variable: %q", match[2])
+			}
+		}
+	}
+	return nil
+}
