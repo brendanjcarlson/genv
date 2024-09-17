db796f43fbf1a36a9289819d30f474e709555a19
Brendan Carlson <brendancarlson1125@gmail.com>
2024-09-17 08:34:20 -0500

    update genv docs

--- genv.go
+++ genv.go
@@ -1,3 +1,41 @@
-// Package genv simplifies working with environment variables through the
-// use of generics.
+// genv is a Go package for loading and accessing environment variables.
+//
+// It simplifies the processes of lookup, casting, and populating configuration structs, using generics, struct tags, and reflection for mapping.
+//
+// Use:
+//
+//	package main
+//
+//	import "github.com/brendanjcarlson/genv"
+//
+//	func main() {
+//	    err := genv.Load(".env")
+//	    if err != nil {
+//	        log.Fatalf("load env: %v", err)
+//	    }
+//
+//	    superSecretKey, err := genv.Get[string]("SUPER_SECRET_KEY")
+//	    if err != nil {
+//	        log.Fatalln(err)
+//	    }
+//
+//	    timeoutSeconds, err := genv.Get[int]("TIMEOUT_SECONDS")
+//	    if errors.Is(err, genv.ErrCannotCast) {
+//	        log.Fatalln(err)
+//	    } else {
+//	        timeoutSeconds = 5
+//	    }
+//
+//	    mustHaveValue := genv.GetOrPanic[string]("GOTTA_HAVE_THIS_ONE")
+//
+//	    type ServerConfig struct {
+//	        Host string `genv:"SERVER_HOST"`
+//	        Port string `genv:"SERVER_PORT"`
+//	    }
+//
+//	    var serverConfig ServerConfig
+//	    if err := genv.GetStruct(&serverConfig); err !=nil {
+//	        log.Fatalln(err)
+//	    }
+//	}
 package genv
